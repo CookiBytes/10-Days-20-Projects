32f153726a555fd059289fbfbbce93344f962a61
Zoe Koh <eden11@protonmail.com>
2020-09-05 22:20:48 +1000

    Day 2 - ToDo App (Clean)

--- todo-react/src/components/form.js
+++ todo-react/src/components/form.js
@@ -9,15 +9,12 @@ function TodoForm({ addTodo }) {
     completed: false,
   });
 
-  function handleTaskInputChange(e) {
-    // e.target.value contains new input from onChange
-    // event for input elements
-    setTodo({ ...todo, task: e.target.value });
+  function handleTaskInputChange(event) {
+    setTodo({ ...todo, task: event.target.value });
   }
 
-  function handleSubmit(e) {
-    e.preventDefault(); // prevents browser refresh
-    // trim() gets rid of string whitespace
+  function handleSubmit(event) {
+    event.preventDefault();
     if (todo.task.trim()) {
       addTodo({ ...todo, id: uuid.v4() });
       setTodo({ ...todo, task: "" });
