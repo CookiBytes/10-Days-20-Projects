bfa99b519ee6f36970c0de2f376a65224fe40586
Zoe Koh <eden11@protonmail.com>
2020-09-12 14:52:17 +1000

    Day 8 - Line Charts

---
+++ Day8/react-charts/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Chart from "./chart";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="chart">
+        <Chart />
+      </div>
+    </div>
+  );
+}
+
+export default App;
