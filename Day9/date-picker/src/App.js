1e2b027e82010c391a58d47401ff1e0c635251c9
Zoe Koh <eden11@protonmail.com>
2020-09-13 17:52:37 +1000

    Day 9 - Date Picker

---
+++ Day9/date-picker/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./App.css";
+
+function App() {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  return (
+    <div className="App">
+      <DatePicker
+        selected={selectedDate}
+        onChange={(date) => setSelectedDate(date)}
+        dateFormat="dd/MM/yyyy"
+        filterDate={(date) => date.getDay() == 6 && date.getDay() == 0}
+        isClearable
+        showYearDropdown
+        showMonthYearDropdown
+      />
+    </div>
+  );
+}
+
+export default App;
